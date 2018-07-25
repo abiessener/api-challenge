ec55eedac1918539bff329c54759826aa2864cf7
Adam Biessener <abiessener@gmail.com>
2018-07-25 18:20:16 -0500

    update scrape script

--- scrape.sh
+++ scrape.sh
@@ -4,12 +4,17 @@
 
 URL_BASE="https://api-challenge-072018.herokuapp.com/products/"
 
+# ex: curl -X POST https://api-challenge-072018.herokuapp.com/products/13860428
+
 startingId=$1;
 endingId=$2;
 
 while [ $startingId -lt $endingId ]; do
-  echo 'startingId is ' $startingId
+
   url=$URL_BASE$startingId
-  curl -X POST $url
+
+  $(curl -X POST $url >> log.txt)
+
   let startingId=startingId+1
+
 done
